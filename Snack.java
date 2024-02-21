e5c09bfaa4e61aae8d1f59656327d57cbc7464a5
b-regan11 <reganbill02@gmail.com>
2024-02-21 17:12:12 -0500

    Can Pick Snacks

--- Snack.java
+++ Snack.java
@@ -1,20 +1,20 @@
 public class Snack {
     private int cost;
     private String name;
-
+    
     public Snack(int cst, String nm){
         if(cst <= 0)
             throw new IllegalArgumentException("Cost must be greater than 0: "+cst);
         cost = cst;
         name = nm;
     }
-    public int getCost() {
+    public int getCost(){
         return cost;
     }
-    public String getName() {
+    public String getName(){
         return name;
     }
-    public String toString() {
+    public String toString(){
         return name + " Cost = $"+cost;
     }
 }
--- VendingMachine.java
+++ VendingMachine.java
@@ -1,15 +1,57 @@
-public class VendingMachine {
-    public void welcome() {
-        System.out.println("Welcome to the Snack Vending Machine");
-        System.out.println("Instructions: ");
-        System.out.println("1. Choose a Snack.");
+import java.util.Scanner;
 
+public class VendingMachine {
+    private Snack selectedSnack;
+
+    public void welcome() {
+        System.out.println("Welcome to the Snack Vending Machine!");
+        System.out.println("Instructions: ");
+        System.out.println("1. Choose a snack. ");
+        
+        System.out.println("\t a -> Cookies ($4)");
+        System.out.println("\t b -> Chips ($3)");
+        System.out.println("\t c -> Granola Bar ($2)");
+        System.out.println("\t d -> Gum ($1)");
+        
         System.out.println("2. Insert $1 bills for payment. ");
         System.out.println("3. Confirm the transaction. ");
         System.out.println("4. Enjoy! ");
     }
+
+    public void selectSnack(char choice) {
+        switch (choice) {
+            case 'a':
+                selectedSnack = new Snack(4, "Cookies");
+                break;
+            case 'b':
+                selectedSnack = new Snack(3, "Chips");
+                break;
+            case 'c':
+                selectedSnack = new Snack(2, "Granola Bar");
+                break;
+            case 'd':
+                selectedSnack = new Snack(1, "Gum");
+                break;
+            default:
+                System.out.println("Invalid choice. Please select a valid snack.");
+                System.exit(0);
+        }
+        if (selectedSnack != null) {
+            System.out.println("Selected Snack: " + selectedSnack);
+        }
+    }
+
+
     public static void main(String[] args) {
         VendingMachine vendingMachine = new VendingMachine();
         vendingMachine.welcome();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Choose a snack (a/b/c/d): ");
+        char snackChoice = scanner.nextLine().toLowerCase().charAt(0);
+
+        vendingMachine.selectSnack(snackChoice);
+
+        scanner.close();
     }
 }
